29ec94c0f9a605f592d831fe17b7306d78422834
AsimTariq <hvorerdu@hotmail.com>
2016-04-04 19:12:48 +0200

    V4 skole og larer

--- lock.php
+++ lock.php
@@ -1,3 +1,4 @@
+<<<<<<< 93f4240bead15f8bc5e915901ff06d20feb55ca8
 <?php
 session_start();
 if(isset($_POST['submit']))
@@ -28,3 +29,35 @@ if(isset($_POST['submit']))
 }
 ?>
 
+=======
+<?php
+session_start();
+if(isset($_POST['submit']))
+{
+ mysql_connect('localhost','root','root') or die(mysql_error());
+ mysql_select_db('lockscreen') or die(mysql_error());
+ $name=$_POST['name'];
+ $pwd=$_POST['pwd'];
+ if($name!=''&&$pwd!='')
+ {
+   $query=mysql_query("select * from login where name='".$name."' and password='".$pwd."'") or die(mysql_error());
+   $res=mysql_fetch_row($query);
+   if($res)
+   {
+    $_SESSION['name']= $name;
+    header('location:index.php');
+   }
+   else
+   {
+    $msg =  'Du har skrevet feil Brukernavn eller Passord, vennligst prøv igjen. ';
+	  header('location:lockscreen.php');
+   }
+ }
+ else
+ {
+  echo'Enter both username and password';
+ }
+}
+?>
+
+>>>>>>> V2
--- logut.php
+++ logut.php
@@ -1,3 +1,4 @@
+<<<<<<< 93f4240bead15f8bc5e915901ff06d20feb55ca8
 <?php
 
 session_start();
@@ -5,4 +6,13 @@ session_destroy();
 
 header("Location: lockscreen.php");
 die();
+=======
+<?php
+
+session_start();
+session_destroy();
+
+header("Location: lockscreen.php");
+die();
+>>>>>>> V2
 exit;?>
